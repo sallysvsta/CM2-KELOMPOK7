e06d1c1e5d4890df52f87bb738d642659a0634a5
najwakussyafira <you@kussyafira@gmail.com>
2025-06-07 21:34:59 +0700

    Najwa

---
+++ CaseMethod2/Dokter.java
@@ -0,0 +1,11 @@
+package CaseMethod2;
+
+public class Dokter {
+    String idDokter;
+    String nama;
+
+    public Dokter(String idDokter, String nama) {
+        this.idDokter = idDokter;
+        this.nama = nama;
+}
+}
---
+++ CaseMethod2/Pasien.java
@@ -0,0 +1,19 @@
+package CaseMethod2;
+
+public class Pasien {
+    String nama;
+    String nik;
+    String keluhan;
+
+    public Pasien(String nama, String nik, String keluhan) {
+        this.nama = nama;
+        this.nik = nik;
+        this.keluhan = keluhan;
+    }
+
+    public void tampilkanInformasi() {
+        System.out.println("Nama Pasien: " + nama);
+        System.out.println("NIK: " + nik);
+        System.out.println("Keluhan: " + keluhan);
+    }
+}
